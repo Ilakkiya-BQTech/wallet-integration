9ffb934cf8a3c2eccb802ae6f8a6037ca1a6ed3f
Ilakkiya-BQTech <ilakkiya@bqtech.in>
2025-03-31 11:00:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ThirdwebProvider, ConnectWallet, metamaskWallet, coinbaseWallet, rainbowWallet, rabbyWallet, zerionWallet, inAppWallet } from "@thirdweb-dev/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThirdwebProvider 
+      clientId=""
+      supportedWallets={[
+        inAppWallet({
+          auth: {
+            options: ["google", "discord", "telegram", "farcaster", "email", "x", "passkey", "phone"],
+          },
+        }),
+        metamaskWallet(),
+        coinbaseWallet(),
+        rainbowWallet(),
+        rabbyWallet(),
+        zerionWallet(),
+      ]}
+    >
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <h1>Welcome to My Web3 App</h1>
+        <ConnectWallet />
+      </div>
+    </ThirdwebProvider>
   );
 }
 
